04226243712af7a960e6c04da963fc716e13937a
ITIS-Kien <nguyentukien.81@gmail.com>
2025-03-26 21:52:12 +0700

    save

--- DSAtest.cpp
+++ DSAtest.cpp
@@ -1,20 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define endl '\n'
-#define mod 1000000007
-#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define fixs(x, n) fixed << setprecision(n) << x
-#define setf(x, n, c) setw(n) << setfill(c) << x
-//ITIS LA NHA
 
-int main(){
-    int t, n; cin >> t;
-    while(t--){
-        cin >> n;
-        int a[n];
-        for (int &it : a) cin >> it;
-        
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define endl "\n"
+const int mod = 1e9 + 7;
+
+int check = 0;
+void Try(int i, int n)
+{
+    if (i == 1 && check == 1)
+    {
+        cout << i << " ";
     }
-    
+    else
+    {
+        if (i == n)
+            check = 1;
+        if (check == 0)
+            Try(i + 1, n);
+        else
+            Try(i - 1, n);
+        cout << i << " ";
+    }
+}
+int main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        check = 0;
+        int n;
+        cin >> n;
+        if(n == 1) cout << 1 << endl;
+        else Try(1, n);
+        cout << endl;
+    }
 }
