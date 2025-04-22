56099f0c6ae5837f5f0ac65e4b5e9f65ba3db29e
ITIS-Kien <nguyentukien.81@gmail.com>
2025-04-22 13:34:05 +0700

    save

--- Polygon/Sol.cpp
+++ Polygon/Sol.cpp
@@ -1,42 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define endl '\n'
 #define MOD 1000000007
-#define BOOST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define BOOST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+#pragma GCC optimize("Ofast")
 
-ll binPow(ll a, ll b) {
-    if (b == 0) return 1;
-    ll res = binPow(a, b / 2);
-    res = (res * res) % MOD;
-    if (b & 1) res = (res * a) % MOD;
-    return res;
+//Hàm chèn số 0 vào xâu ít kí tự hơn 
+//Để đảm bảo độ dài 2 xâu bằng nhau
+void len(string &a, string &b) {
+	int lena = a.size(), lenb = b.size();
+	if(lena < lenb) a = string(lenb - lena, '0') + a;
+	else if(lena > lenb) b = string(lena - lenb, '0') + b;
 }
 
+//Hàm tìm vị trí số 1 đầu tiền
+//Để loại bỏ số 0 thừa phía trước
+string era(string s){
+	int i = 0;
+	while(i < s.size() && s[i] == '0') ++i;
+	if(i == s.size()) return "0";
+	return s.substr(i);
+}
 int main() {
     BOOST;
-    ll n, m = 1; 
-    cin >> n;
-    vector<ll> a(n);
-    unordered_map<ll, int> freq;
-
-    for (ll &it : a) {
-        cin >> it;
-        ++freq[it];
-    }
-
-    const int mod_2phi = 2 * (MOD - 1);
-    for (auto &[p, cnt] : freq) {
-        m = (m * (cnt + 1)) % mod_2phi;
-    }
-
-    ll ans = 1;
-    for (auto &[p, cnt] : freq) {
-        ll exponent = ( (cnt % mod_2phi) * (m % mod_2phi) ) % mod_2phi;
-        exponent = exponent / 2; 
-        exponent %= (MOD - 1); 
-        ans = (ans * binPow(p, exponent)) % MOD;
-    }
-
-    cout << ans;
-    return 0;
+    int t;
+	cin >> t;
+	cin.ignore();
+	while(t--) {
+		string s1, s2, ans = "";
+		string s;
+		cin >> s >> s1;
+        // Nếu NOT thì chỉ nhập xâu s1
+		if(s != "NOT") {
+			cin >> s2;
+            // Đảm bảo độ dài 2 xâu bằng nhau
+			len(s1, s2);
+            // Duyệt từ cuối xâu đi lên
+			for(int i = s1.size() - 1; i >= 0; --i) {
+				if(s == "AND") {
+                    // Phép toàn AND bằng 1 khi và chỉ khi cả 2 bit bằng 1
+                    if(s1[i] == '1' and s2[i] == '1') ans = "1" + ans;
+                    else ans = "0" + ans;
+                } else if(s == "OR") {
+                    // Phép toàn OR bằng 0 khi và chỉ khi cả 2 bit bằng 0
+                    if(s1[i] == '0' and s2[i] == '0') ans = "0" + ans;
+                    else ans = "1" + ans;
+                } else {
+                    // Phép toàn XOR bằng 0 khi và chỉ khi cả 2 bit bằng nhau
+                    if(s1[i] == s2[i]) ans = "0" + ans;
+                    else ans = "1" + ans;
+                }
+			}
+		} else { // Phép toán NOT là phép toán đảo bit, bit 1 thành 0 và ngược lại
+			for(int i = s1.size() - 1; i >= 0; --i) {
+                if (s1[i] == '0') ans = "1" + ans;
+                else ans = "0" + ans;
+			}
+		}
+        // Loại bỏ các số 0 thừa ở đầu xâu
+		cout << era(ans) << '\n';
+	}
 }
